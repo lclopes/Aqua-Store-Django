9712b934c3ca9551699d68f9ab26afa896b11e84
Lucas Lopes <lucascastro@id.uff.br>
2019-11-18 11:54:21 -0200

    Added admin app; Refactored app and project names

---
+++ aquastore_admin/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AquastoreAdminConfig(AppConfig):
+    name = 'aquastore_admin'
---
+++ aquastore_app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, get_object_or_404
+from .models import CategoriaAguas, Agua
+import os
+
+
+def lista_aguas(request, slug_da_categoria=None):
+   categoria = None
+   categorias = CategoriaAguas.objects.all().order_by("nome")
+   aguas = Agua.objects.filter(disponivel=True).order_by("nome")
+   if slug_da_categoria:
+      categoria = get_object_or_404(CategoriaAguas, slug=slug_da_categoria)
+      aguas = aguas.filter(categoria=categoria).order_by("nome")
+
+   return render(request, 'aquastore_app/lista.html', {'categorias': categorias,
+                                                 'aguas': aguas,
+                                                 'categoria': categoria})
+
+
+def exibe_agua(request, id, slug_da_agua):
+   agua = get_object_or_404(Agua, id=id)
+   return render(request, 'aquastore_app/exibe.html', {'agua': agua})
+
+
+def sobre(request):
+    return render(request, 'aquastore_app/sobre.html')
+
+def cadastro(request):
+    return render(request, 'aquastore_app/cadastro.html')
