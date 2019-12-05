752b0440cf6c42e4ecf655860f1cfc98e08a98cc
Lucas de C. Lopes <lucascastro@id.uff.br>
2019-12-05 18:32:34 -0300

    Added pagination

--- aquastore_app/views.py
+++ aquastore_app/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 from .models import CategoriaAguas, Agua
-import os
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def lista_aguas(request, slug_da_categoria=None):
    categoria = None
    categorias = CategoriaAguas.objects.all().order_by("nome")
    aguas = Agua.objects.filter(disponivel=True).order_by("nome")
+
    if slug_da_categoria:
       categoria = get_object_or_404(CategoriaAguas, slug=slug_da_categoria)
       aguas = aguas.filter(categoria=categoria).order_by("nome")
 
+   paginator = Paginator(aguas, 8)
+   page = request.GET.get('page')
+   try:
+       aguas_lista = paginator.page(page)
+   except PageNotAnInteger:
+       # If page is not an integer deliver the first page
+       aguas_lista = paginator.page(1)
+   except EmptyPage:
+       # If page is out of range deliver last page of results
+       aguas_lista = paginator.page(paginator.num_pages)
+
    return render(request, 'aquastore_app/lista.html', {'categorias': categorias,
-                                                 'aguas': aguas,
+                                                       'page':page,
+                                                 'aguas_lista': aguas_lista,
                                                  'categoria': categoria})
 
 def exibe_agua(request, id, slug_da_agua):
